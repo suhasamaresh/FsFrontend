34bed94501a4b7eb04b35c9db20f2f232197163c
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-07-19 14:50:09 -0700

    adding in-lined default secrets - window.location will not work with nextjs

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -13,34 +13,33 @@ const Providers = (props: { children: ReactNode }) => {
   const chains = [mainnet, polygon] as [Chain, ...Chain[]]
 
   // Get your own keys on sequence.build
-  const projectAccessKey = process.env.NEXT_PUBLIC_PROJECT_ACCESS_KEY
-  const waasConfigKey = process.env.NEXT_PUBLIC_WAAS_CONFIG_KEY
-  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
-  const appleClientId = process.env.NEXT_PUBLIC_APPLE_CLIENT_ID
-  /* This is disabled for now */
-  /* const appleRedirectURI = window.location.origin + window.location.pathname */
-  const walletConnectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID
+  const projectAccessKey = process.env.NEXT_PUBLIC_PROJECT_ACCESS_KEY || 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
+  const waasConfigKey = process.env.NEXT_PUBLIC_WAAS_CONFIG_KEY || 'eyJwcm9qZWN0SWQiOjE2ODE1LCJlbWFpbFJlZ2lvbiI6ImNhLWNlbnRyYWwtMSIsImVtYWlsQ2xpZW50SWQiOiI2N2V2NXVvc3ZxMzVmcGI2OXI3NnJoYnVoIiwicnBjU2VydmVyIjoiaHR0cHM6Ly93YWFzLnNlcXVlbmNlLmFwcCJ9'
+  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com'
+  // const appleClientId = process.env.NEXT_PUBLIC_APPLE_CLIENT_ID || 'com.horizon.sequence.waas'
+  // const appleRedirectURI = window.location.origin + window.location.pathname //this approach doesn't work with nextjs
+  const walletConnectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || 'c65a6cb1aa83c4e24500130f23a437d8'
 
   if (!projectAccessKey) {
     throw new Error('projectAccessKey is not defined');
   }
-  
+
   if (!waasConfigKey) {
     throw new Error('waasConfigKey is not defined');
   }
-  
+
   if (!googleClientId) {
     throw new Error('googleClientId is not defined');
   }
-  
-  if (!appleClientId) {
-    throw new Error('appleClientId is not defined');
-  }
-  
-  /* if (!appleRedirectURI) {
-    throw new Error('appleRedirectURI is not defined');
-  } */
-  
+
+  // if (!appleClientId) {
+  //   throw new Error('appleClientId is not defined');
+  // }
+
+  // if (!appleRedirectURI) {
+  //   throw new Error('appleRedirectURI is not defined');
+  // }
+
   if (!walletConnectId) {
     throw new Error('walletConnectId is not defined');
   }
@@ -49,7 +48,7 @@ const Providers = (props: { children: ReactNode }) => {
     walletConnectProjectId: walletConnectId,
     waasConfigKey,
     googleClientId,
-    // Notice: uncomment to use Apple if deployed on https to support Apple redirects
+    // Notice: AppleID will only work if deployed on https to support Apple redirects
     // appleClientId,
     // appleRedirectURI,
     defaultChainId: 137,
